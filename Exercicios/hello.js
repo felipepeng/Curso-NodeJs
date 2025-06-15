6c543df88cf6e1d902220fd89db7cd1e193fede2
Peng <felipebarretocortes.fb@gmail.com>
2025-06-15 02:45:10 -0300

    ajuste

--- Exercicios/hello.js
+++ Exercicios/hello.js
@@ -1,5 +1,5 @@
 let hello = "Hello, World! How it is going Peng?";
 console.log(hello);
 
-hello = hello.replace("Peng", "Penguin");
+hello = hello.replace("Peng", "Penguinini");
 console.log(hello);
